0a8512a7ef3e6c1dc9727223ea4564fc66c8895f
SatoJoe762 <satocripto25@gmail.com>
2024-08-04 12:42:12 +1200

    Ultima modificacion

--- packages/hardhat/contracts/MyNft.sol
+++ packages/hardhat/contracts/MyNft.sol
@@ -9,7 +9,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	uint256 private _nextTokenId;
 
-	constructor() ERC721("Selena", "SLN") Ownable() {}
+	constructor(address initialOwner)
+        ERC721("CursoSolidity2024ETHB", "MTK")
+        Ownable()
+    {
+        transferOwnership(initialOwner);
+        mintInitialTokens();
+    }
+	function mintInitialTokens() internal onlyOwner {
+        _mintAndSetURI(0x70997970C51812dc3A010C7d01b50e0d17dc79C8, "https://gateway.pinata.cloud/ipfs/QmQk77ZDV7HXaBLBKJvPz5p8XxFocuJzASoXadw6ZQgpPt");
+        _mintAndSetURI(0x90F79bf6EB2c4f870365E785982E1f101E93b906, "https://gateway.pinata.cloud/ipfs/QmZkpjKyQpUYFCj9vta7EQ6Gt68kXdPgBGprpvMLnZ1fCf");
+		
+    }
+
+    function _mintAndSetURI(address to, string memory uri) internal {
+        safeMint(to, uri);
+    }
 
 	function safeMint(address to, string memory uri) public onlyOwner {
 		uint256 tokenId = _nextTokenId++;
@@ -18,25 +33,29 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	}
 
 	// The following functions are overrides required by Solidity.
+	function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
-	function tokenURI(
-		uint256 tokenId
-	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-		return super.tokenURI(tokenId);
-	}
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-	function supportsInterface(
-		bytes4 interfaceId
-	) public view override(ERC721) returns (bool) {
-		return super.supportsInterface(interfaceId);
-	}
+    function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
 
-	function _burn(
-		uint256 tokenId
-	) internal override(ERC721, ERC721URIStorage) {
-		super._burn(tokenId);
-	}
-	function buyNFT(uint256 tokenId) public payable {
+    function buyNFT(uint256 tokenId) public payable {
         require(msg.value >= 0.1 ether, "Insufficient payment");
         address owner = ownerOf(tokenId);
         require(owner != msg.sender, "Cannot buy your own NFT");
